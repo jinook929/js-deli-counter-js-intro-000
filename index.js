d7b14b66b313a4e9844f964c661a49843ac05ef1
JINOOK JUNG <jinook929@gmail.com>
2020-11-07 02:03:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,8 @@ function takeANumber(arr, name) {
   arr.push(name);
   return `Welcome, ${name}. You are number ${arr.length} in line.`;
 }
+
+function currentLine(arr) {
+  let str = ""
+  return 
+}
